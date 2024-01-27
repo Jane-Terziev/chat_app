a0585e63b18370ec6cbc36e330b8f4bda4e535d2
Jane-Terziev <janeterziev@gmail.com>
2024-01-28 00:00:23 +0100

    Added add chat participant validator spec

---
+++ chats/spec/ui/add_chat_participant_validator_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Chats::Ui::AddChatParticipantValidator, type: :unit do
+  describe '#.call(params)' do
+    let(:params) do
+      {
+        user_ids: [SecureRandom.uuid]
+      }
+    end
+
+    context 'when all of the required parameters are present' do
+      it 'should return success true' do
+        expect(described_class.new.call(params).success?).to be_truthy
+      end
+    end
+
+    context "when the user_ids parameter is missing" do
+      it 'should raise an error' do
+        expect(described_class.new.call(params.except(:user_ids)).success?).to be_falsey
+      end
+    end
+
+    context "when the user_ids parameter is empty" do
+      it 'should raise an error' do
+        params[:user_ids] = []
+        expect(described_class.new.call(params).success?).to be_falsey
+      end
+    end
+  end
+end
