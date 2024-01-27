f7be3dde1ec16684f76f8303dfadfec66d4925bf
Jane-Terziev <janeterziev@gmail.com>
2024-01-27 23:54:44 +0100

    Fixed a bug in validation when user_ids is empty

--- chats/lib/chats/ui/create_chat_validator.rb
+++ chats/lib/chats/ui/create_chat_validator.rb
@@ -3,7 +3,7 @@ module Chats
     class CreateChatValidator < ApplicationContract
       params do
         required(:name).value(Types::StrippedString, :filled?)
-        required(:user_ids).value(:array?).each(:string)
+        required(:user_ids).value(:array?, min_size?: 1).each(:string)
       end
     end
   end
--- chats/spec/ui/create_chat_validator_spec.rb
+++ chats/spec/ui/create_chat_validator_spec.rb
@@ -15,9 +15,22 @@ RSpec.describe Chats::Ui::CreateChatValidator, type: :unit do
       end
     end
 
-    context 'when a required parameter is missing' do 
-      it 'should raise an error when name is missing' do
+    context 'when the name parameter is missing' do
+      it 'should raise an error' do
         expect(described_class.new.call(params.except(:name)).success?).to be_falsey
+      end
+    end
+
+    context "when the user_ids parameter is missing" do
+      it 'should raise an error' do
+        expect(described_class.new.call(params.except(:user_ids)).success?).to be_falsey
+      end
+    end
+
+    context "when the user_ids parameter is empty" do
+      it 'should raise an error' do
+        params[:user_ids] = []
+        expect(described_class.new.call(params).success?).to be_falsey
       end
     end
   end
