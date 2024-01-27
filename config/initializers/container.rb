7346864607b954d7f3cb583e9dc85ab66093fa8c
Jane-Terziev <janeterziev@gmail.com>
2024-01-28 00:00:12 +0100

    Modified rails event store to execute domain events without sending them to sidekiq in test env

--- config/initializers/container.rb
+++ config/initializers/container.rb
@@ -10,26 +10,37 @@ module App
   class Container < Dry::System::Container
     register('contract_validator') { ContractValidator.new }
     register('current_user_repository') { CurrentUserRepository }
-    register('events.client', RailsEventStore::Client.new(
-      repository: NoOpEventRepository.new,
-      mapper:     ToYAMLEventMapper.new,
-      dispatcher: RubyEventStore::ComposedDispatcher.new(
-        RailsEventStore::ImmediateAsyncDispatcher.new(scheduler: ActiveJobEventScheduler.new(serializer: YAML)),
-        RailsEventStore::ImmediateAsyncDispatcher.new(
-          scheduler: AsyncThreadEventScheduler.new(
-            job_scheduler:      AsyncJobScheduler.new(
-              executor_service: Concurrent::ThreadPoolExecutor.new(
-                min_threads:     1,
-                max_threads:     (ENV['BACKGROUND_THREADS'] || 5).to_i,
-                max_queue:       ENV['BACKGROUND_TASK_POOL_SIZE'].to_i,
-                fallback_policy: :caller_runs
-              )
-            ),
-            event_deserializer: YAML
+    if Rails.env.test?
+      register('events.client', RailsEventStore::Client.new(
+        repository: NoOpEventRepository.new,
+        mapper:     ToYAMLEventMapper.new,
+        dispatcher: RubyEventStore::ComposedDispatcher.new(
+          RailsEventStore::ImmediateAsyncDispatcher.new(scheduler: ImmediateActiveJobEventScheduler.new(serializer: YAML)),
+          RailsEventStore::ImmediateAsyncDispatcher.new(scheduler: ImmediateEventScheduler.new(event_deserializer: YAML))
+        )
+      ))
+    else
+      register('events.client', RailsEventStore::Client.new(
+        repository: NoOpEventRepository.new,
+        mapper:     ToYAMLEventMapper.new,
+        dispatcher: RubyEventStore::ComposedDispatcher.new(
+          RailsEventStore::ImmediateAsyncDispatcher.new(scheduler: ActiveJobEventScheduler.new(serializer: YAML)),
+          RailsEventStore::ImmediateAsyncDispatcher.new(
+            scheduler: AsyncThreadEventScheduler.new(
+              job_scheduler:      AsyncJobScheduler.new(
+                executor_service: Concurrent::ThreadPoolExecutor.new(
+                  min_threads:     1,
+                  max_threads:     (ENV['BACKGROUND_THREADS'] || 5).to_i,
+                  max_queue:       ENV['BACKGROUND_TASK_POOL_SIZE'].to_i,
+                  fallback_policy: :caller_runs
+                )
+              ),
+              event_deserializer: YAML
+            )
           )
         )
-      )
-    ))
+      ))
+    end
 
     register('events.publisher', memoize: true) { MetadataEventPublisher.new }
   end
